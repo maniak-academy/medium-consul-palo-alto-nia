d10ff017fc5237b6d2d2bd0250ccd51b6fa4781d
Sebastian Maniak <sebastian@maniak.io>
2022-07-12 23:23:31 -0400

    up

--- 02-configure-infra/pan-config/security-policies.tf
+++ 02-configure-infra/pan-config/security-policies.tf
@@ -46,6 +46,22 @@ resource "panos_security_rule_group" "allow_traffic" {
     }
 }
 
+resource "panos_security_rule_group" "out_traffic" {
+    position_keyword = "top"
+    #position_reference = panos_security_rule_group.deny_all.rule.0.name
+    rule {
+        name = "Allow outoging traffic"
+        source_zones = [panos_zone.private_zone.name]
+        source_addresses = ["any"]
+        source_users = ["any"]
+        destination_zones = [panos_zone.public_zone.name]
+        destination_addresses = ["any"]
+        applications = ["any"]
+        services = ["any"]
+        categories = ["any"]
+        action = "allow"
+    }
+}
 
 # resource "panos_security_rule_group" "deny_all" {
 #     position_keyword = "bottom"
